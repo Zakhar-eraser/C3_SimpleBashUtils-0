83d03dfbb66dc2198b9484cfe30a0098d6aa77ce
Zakhar-eraser <79922187361@yandex.ru>
2022-11-15 01:02:00 +0300

    parser done

--- src/grep/s21_grep.c
+++ src/grep/s21_grep.c
@@ -1,31 +1,63 @@
 #include "s21_grep.h"
 
 int main(int argc, char **argv) {
-    int out = 0, pcre_opts;
-    char *pattern = get_pattern(argc, argv);
-    if (pattern) {
+    int out = 1, erroroffset;
+    const char *error;
+    pcre *re;
+    match_modifiers mods = {.argc = argc, .argv = argv};
+    char *pattern = get_pattern(&mods);
+    if (pattern && (optind < argc)) {
+        if ((re = pcre_compile(pattern, mods.pcre_opts,
+            &error, &erroroffset, NULL))) {
+            for (int i = optind; i < argc; i++) {
+                FILE *file;
+                if ((file = fopen(argv[i], "r"))) {
 
+                } else {
+                    if (!mods.hide_filenames)
+                        printf("No such file: %s\n", argv[i]);
+                }
+            }
+            printf("Pattern compilation error at %d: %s", erroroffset, error);
+        }
     } else {
         printf("Wrong options");
     }
+    return out;
 }
 
-char *get_pattern(int argc, char **argv, int *pcre_opts) {
+int find_matches(match_modifiers *mods, FILE *file) {
+
+}
+
+char *get_pattern(match_modifiers *mods) {
     int opt;
-    char *options = "e:ivclnhsf:o";
-    char *pattern = "";
-    while ((opt = getopt(argc, argv, options)) != -1) {
-        switch (opt) {
-        case 'e':
+    char *options = "e:ivclnhsf:o", *pattern = "";
+    while ((opt = getopt(mods->argc, mods->argv, options)) != -1) {
+        if (opt == 'e') {
             extend_pattern(pattern, optarg);
-            break;
-        case 'i':
-            *pcre_opts = PCRE_CASELESS;
-            break;
-        case 
-        default:
-            out = 1;
-            break;
+        } else if (opt == 'i') {
+            mods->pcre_opts = PCRE_CASELESS;
+        } else if (opt == 'v') {
+            mods->inversion = 1;
+        } else if (opt == 'c') {
+            mods->only_matches_count = 1;
+        } else if (opt == 'l') {
+            mods->first_match = 1;
+        } else if (opt == 'n') {
+            mods->print_line_number = 1;
+        } else if (opt == 'h') {
+            mods->hide_filenames = 1;
+        } else if (opt == 's') {
+            mods->hide_warnings = 1;
+        } else if (opt == 'f') {
+            extend_pattern_from_file(pattern, optarg);
+        } else if (opt == 'o') {
+            mods->all_matches = 1;
+        } else {
+            free(pattern);
+            pattern = NULL;
         }
     }
+    return pattern;
 }
--- src/grep/s21_grep.h
+++ src/grep/s21_grep.h
@@ -5,8 +5,25 @@
 #include <unistd.h>
 #include <pcre.h>
 
-void extend_pattern(char *old, char *add);
+typedef struct match_modifiers {
+    int argc;
+    char **argv;
+    int pcre_opts;
+    int inversion;
+    size_t matches_counter;
+    size_t lines_counter;
+    int only_matches_count;
+    int print_line_number;
+    int first_match;
+    int hide_filenames;
+    int hide_warnings;
+    int all_matches;
+} match_modifiers;
 
-char *get_pattern(int argc, char **argv, int *pcre_opts);
+void extend_pattern(char *old, char *add);
+void extend_pattern_from_file(char *old, char *filename);
+
+char *get_pattern(match_modifiers *mods);
+int find_matches(match_modifiers *mods, FILE *file);
 
 #endif  // _SRC_GREP_S21_GREP_H_
